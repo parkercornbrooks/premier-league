c20d740ebe8834d9285c5d519a1d841d5168579f
Parker Cornbrooks <pcorn123@gmail.com>
2024-03-30 23:29:11 -0400

    feat: set up server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+const PORT = ":8000"
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(PORT, nil))
 }
