fc5b54831ac0d1c7305abad30b289830c70bf0f1
Parker Cornbrooks <pcorn123@gmail.com>
2024-03-30 23:35:40 -0400

    feat: fetch team data

--- main.go
+++ main.go
@@ -1,14 +1,54 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
 
-const PORT = ":8000"
+const (
+	PORT     = ":8000"
+	TEAM_URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
+)
+
+type Team struct {
+	Code      int    `json:"code"`
+	Name      string `json:"name"`
+	ShortName string `json:"short_name"`
+}
+
+type TeamData struct {
+	Teams []Team `json:"teams"`
+}
+
+func fetchTeamData() (TeamData, error) {
+	resp, err := http.Get(TEAM_URL)
+	if err != nil {
+		return TeamData{}, err
+	}
+	defer resp.Body.Close()
+
+	var d TeamData
+
+	err = json.NewDecoder(resp.Body).Decode(&d)
+	if err != nil {
+		return TeamData{}, err
+	}
+	return d, nil
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	premData, err := fetchTeamData()
+	if err != nil {
+		fmt.Fprintf(w, "error getting team data")
+	}
+	data, err := json.Marshal(premData)
+	if err != nil {
+		fmt.Fprintf(w, "error marshalling data")
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
 }
 
 func main() {
